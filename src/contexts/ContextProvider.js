fabfaa1db492dfba8ced28cc5b939eff008f5892
ZnoGouDj <znogoud3@gmail.com>
2022-05-21 18:43:59 +0200

    Navbar

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,8 +11,20 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
 
-  return <StateContext.Provider value={{ activeMenu, setActiveMenu }}>{children}</StateContext.Provider>;
+  const handleClick = clicked => {
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
+
+  return (
+    <StateContext.Provider
+      value={{ activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
 };
 
 export const useStateContext = () => useContext(StateContext);
